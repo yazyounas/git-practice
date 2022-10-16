4a92b305e85082b309a275936685fcbe2519e41d
Yasir Younas <yazyounas@gmail.com>
2022-10-16 14:56:13 +0100

    javascript Practice

---
+++ practice.js
@@ -0,0 +1,52 @@
+/*
+//const s = 'Hello, World!'
+//console.log(s.split(', '))
+
+// Arrays - varibale that hold multiple value 
+//const numbers = new Array(1,2,3,4,5);
+//console.log(numbers)
+
+//push method add new element at the end of an array
+//ushift method add new element at the start of an array
+// pop methodf 
+
+//obecjt literla 
+const todos = [
+    {
+        id: 1,
+        text: 'take out trash',
+        isCompleted : true
+    }
+    
+];
+
+//covert to jSON
+//const todoJSON = JSON.stringify(todos);
+//console.log(todoJSON);
+
+//For of loop
+//for (let todo of todos) {
+ //   console.log(todo.text);
+//}
+/*
+// While Loop
+ let i = 0;
+ while (i <= 10) {
+    console.log(`while Loop Number: ${i}`);
+    i++;
+ }
+ */
+/*
+const todoCompleted = todos.filter((todo) => {
+ return todo.isCompleted === true;
+}).map((todo) =>{
+    return todo.text;
+})
+console.log(todoCompleted);
+*/
+const x = 100;
+if(x === 10) {
+    console.log('x is 10')
+} else {
+    console.log('x is not 10')
+}
